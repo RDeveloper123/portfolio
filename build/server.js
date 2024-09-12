18d44f0090f9ff0ba48a63814b9e3c69e1b2763b
RDeveloper123 <vasimakramc@gmail.com>
2024-09-12 18:02:01 +0530

    backend updated

--- build/server.js
+++
@@ -1,2 +0,0 @@
-"use strict";const s=require("express"),e=s();require("dotenv").config();const r=require("cors"),o=require("./controller/sendmail");e.use(r());e.use(s.json());e.post("/contact",o.mail);e.listen(process.env.PORT||5e3,()=>console.log(`server started on ${process.env.PORT}`));
-//# sourceMappingURL=server.js.map
